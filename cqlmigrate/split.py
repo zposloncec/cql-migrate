6932e50d7e880a72e737a78f537e1253f0ae13a1
Phil Wise <phil@advancedtelematic.com>
2014-11-10 14:24:28 +0100

    Now can split all required types of CQL

--- cqlmigrate/split.py
+++ cqlmigrate/split.py
@@ -1,37 +1,79 @@
-from pyparsing import alphanums, lineEnd, ParseResults
-from pyparsing import CaselessLiteral, Regex, Word, Forward
+from pyparsing import alphanums, nums, lineEnd, ParseResults
+from pyparsing import CaselessLiteral, Regex, Word, Forward, QuotedString
 from pyparsing import nestedExpr, Optional, OneOrMore
 
 comment = Regex('--[^\n]*') + lineEnd
 
-identifier = Word(alphanums + '_.')
-colnamelist = Forward()
-colnamelist << ((identifier + ',' + colnamelist) | identifier)
+def itemlist(start, body, delimiter, end):
+    a = Forward()
+    a << ((body + delimiter + a) | body)
+    return nestedExpr(opener=start, closer=end, content=a, ignoreExpr=None)
 
-pkey = CaselessLiteral("PRIMARY") + CaselessLiteral("KEY") + nestedExpr(content=colnamelist)
+identifier = Word(alphanums + '_.')
+
+pkey = CaselessLiteral("PRIMARY") + CaselessLiteral("KEY") + itemlist('(', identifier, ',', ')')
 
 cqltype = Word(alphanums + '_<>.')
 coldefn = pkey | (identifier + cqltype)
-coldefnlist = Forward()
-coldefnlist << ((coldefn + ',' + coldefnlist) | coldefn )
+
+ifnotexists = Optional(CaselessLiteral("IF") + CaselessLiteral("NOT") + CaselessLiteral("EXISTS"))
+
+literal = Forward()
+literal_string = QuotedString("'")
+
+map_kv = literal_string + ':' + literal
+literal_map = itemlist('{', map_kv, ',', '}')
+
+literal_int = Word(nums)
+literal << (literal_string | literal_int | literal_map )
 
 
-ctable = (CaselessLiteral("CREATE") + CaselessLiteral("TABLE") +
-          Optional(CaselessLiteral("IF") +
-                             CaselessLiteral("NOT") + 
-                             CaselessLiteral("EXISTS"))
-          + identifier + 
-          nestedExpr(content=coldefnlist)
-          + ';'
-          )
+class ParseActionSimple(object):
+    def __init__(self, tagas):
+        self.tagas = tagas
+    def __call__(self, s, loc, tocs):
+        return ParseResults([(self.tagas, None, loc)])
+
+# CREATE TABLE ...
+ctable = (CaselessLiteral("CREATE") + CaselessLiteral("TABLE") + ifnotexists
+          + identifier + itemlist('(', coldefn, ',', ')') + ';')
+
+ctable.setParseAction(ParseActionSimple('CREATE'))
+
+# ALTER TABLE ....
+alter = (CaselessLiteral("ALTER") + CaselessLiteral("TABLE") + identifier + CaselessLiteral("ADD") +
+        identifier + cqltype + ";")
+
+alter.setParseAction(ParseActionSimple('ALTER'))
 
 
-def parseAction(s, loc, tocs):
-    return ParseResults([('CREATE', loc)])
+# UPDATE ....
+update = (CaselessLiteral("UPDATE") + identifier + CaselessLiteral("SET") + identifier + '=' + literal + 
+        CaselessLiteral("WHERE") + identifier + '=' + literal + ';')
 
-ctable.setParseAction(parseAction)
+class UpdateStatement(object):
+    def __init__(self, table, pkcol, pkvalue, col, value):
+        self.table = table
+        self.pkcol = pkcol
+        self.pkvalue = pkvalue
+        self.col = col
+        self.value = value
+    def __str__(self):
+        return "UPDATE table:%s pkey:%s=%s col:%s value:%s" % (self.table, self.pkcol, self.pkvalue, self.col, self.value)
 
-cql = OneOrMore(ctable)
+def updateParseAction(s, loc, tocs):
+    info = UpdateStatement(tocs[1], tocs[7], tocs[9], tocs[3], tocs[5])
+    return ParseResults([('UPDATE', info, loc)])
+
+update.setParseAction(updateParseAction)
+
+# CREATE KEYSPACE
+create_keyspace = (CaselessLiteral("CREATE") + CaselessLiteral("KEYSPACE") + ifnotexists + identifier
+        + CaselessLiteral("WITH") + identifier + '=' + literal_map + ';')
+
+create_keyspace.setParseAction(ParseActionSimple('CREATE KEYSPACE'))
+
+cql = OneOrMore(ctable | alter | update | create_keyspace)
 
 cql.ignore(comment)
 cql.enablePackrat()
@@ -39,18 +81,20 @@ cql.parseWithTabs()
 
 
 class CqlChunk(object):
-    def __init__(self, src, start, end):
+    def __init__(self, src, chunk_type, info, start, end):
         self.src = src
+        self.chunk_type = chunk_type
+        self.info = info
         self.start = start
         self.end = end
     def body(self):
         return self.src[self.start:self.end].strip()
-    def is_create(self):
-        return True
+    def is_update(self):
+        return self.chunk_type == 'UPDATE'
 
 def splitCql(s):
     chunk_breaks = cql.parseString(s, parseAll=True)
-    res = [CqlChunk(s, start, None) for t,start in chunk_breaks]
+    res = [CqlChunk(s, chunk_type, info, start, None) for chunk_type, info, start in chunk_breaks]
     for i in xrange(0,len(res)-1):
         res[i].end = res[i+1].start
     return res
--- test/simple.py
+++ test/simple.py
@@ -2,6 +2,7 @@
 
 import unittest
 from unittest import TestCase
+from os.path import join, dirname
 
 import cqlmigrate
 
@@ -32,12 +33,54 @@ simple_res = ["""CREATE TABLE IF NOT EXISTS auth_plus.clients (
 );"""]
 
 
+alter = """
+alter table bar add bax text;
+alter table bar add ggg text;
+"""
+
+alter_res = [ "alter table bar add bax text;", "alter table bar add ggg text;" ]
+
+
+update = """
+update tok set details='asdf' WHERE name='bob';
+alter table bar add ggg text;
+"""
+
+update_res = [ "update tok set details='asdf' WHERE name='bob';", "alter table bar add ggg text;" ]
+
+
+keyspace = """
+CREATE KEYSPACE IF NOT EXISTS my_keyspace
+WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 2};
+alter table bar add ggg text;
+"""
+
+
 class SplitCQL(TestCase):
-    def testSimpleSplit(self):
+    def testCreateSplit(self):
         res = cqlmigrate.splitCql(simple)
         body = [x.body() for x in res]
         self.assertEquals(body, simple_res)
-        self.assertEquals([i.is_create() for i in res], [True, True])
+        self.assertEquals([x.is_update() for x in res], [False, False])
+    def testAlterSplit(self):
+        res = cqlmigrate.splitCql(alter)
+        body = [x.body() for x in res]
+        self.assertEquals(body, alter_res)
+        self.assertEquals([x.is_update() for x in res], [False, False])
+    def testUpdate(self):
+        res = cqlmigrate.splitCql(update)
+        body = [x.body() for x in res]
+        self.assertEquals(body, update_res)
+        self.assertEquals([x.is_update() for x in res], [True, False])
+        u = res[0].info
+        self.assertEquals(u.table, 'tok')
+        self.assertEquals(u.col, 'details')
+        self.assertEquals(u.value ,'asdf')
+        self.assertEquals(u.pkcol ,'name')
+        self.assertEquals(u.pkvalue ,'bob')
+    def testKeyspace(self):
+        res = cqlmigrate.splitCql(keyspace)
+        self.assertEquals(len(res), 2)
 
 if __name__ == '__main__':
     unittest.main()
